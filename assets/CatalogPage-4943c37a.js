874a738fadc65d129cc110758203c1da0fdaff86
OksanaKorsun <128100267+OksanaKorsun@users.noreply.github.com>
2024-04-18 09:03:27 +0000

    Deploying to gh-pages from @ OksanaKorsun/campers-for-rent@9aabc35e63a35fc2c1bc0bed2e9b552244a8e0d0 🚀

---
+++ assets/CatalogPage-4943c37a.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-0cfc1c23.js";const e=()=>a.jsx("div",{children:"Catalog Page"});export{e as default};
---
+++ assets/HomePage-b4b75fe9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-0cfc1c23.js";const r=()=>e.jsx("div",{children:"Home Page"});export{r as default};
